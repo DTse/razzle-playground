15c23ee3a63eccb8bc612ba2e7b873388fcc7679
Dimitris Tsetsonis <dimitristsetsonis@MacBook-Pro.station>
2020-10-04 16:13:21 +0300

    feat: create sidebar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React, { memo } from 'react';
-import clsx from 'clsx';
-import {useAppContext} from '../context/AppContext';
-import {withRouter} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { memo } from "react";
+import clsx from "clsx";
+import { useAppContext } from "../context/AppContext";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
 
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
     appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
+        transition: theme.transitions.create(["margin", "width"], {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
         }),
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     appBarShift: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
+        transition: theme.transitions.create(["margin", "width"], {
             easing: theme.transitions.easing.easeOut,
             duration: theme.transitions.duration.enteringScreen,
         }),
@@ -34,27 +34,27 @@ const useStyles = makeStyles(theme => ({
         marginRight: theme.spacing(2),
     },
     hide: {
-        display: 'none',
+        display: "none",
     },
     headerTitle: {
         fontSize: 26,
-        textTransform: "capitalize"
-    }
+        textTransform: "capitalize",
+    },
 }));
 
 /**
-   * Return the App Bar component.
-   * @param {object} location
-   * @return {any} img
-**/
-const Header = memo(({location}) => {
+ * Return the App Bar component.
+ * @param {object} location
+ * @return {any} img
+ **/
+const Header = memo(({ location }) => {
     const classes = useStyles();
     const { state, dispatch } = useAppContext();
 
-    const title = location.pathname.replace(/\//g,'');
+    const title = location.pathname.replace(/\//g, "");
 
     const handleDrawerOpen = () => {
-        dispatch({type: 'setOpen', value: true});
+        dispatch({ type: "setOpen", value: true });
     };
 
     return (
@@ -75,17 +75,15 @@ const Header = memo(({location}) => {
                     <MenuIcon />
                 </IconButton>
                 <Typography className={classes.headerTitle} variant="h1" noWrap>
-                    { title ? title : 'Home' }
+                    {title ? title : "Home"}
                 </Typography>
             </Toolbar>
         </AppBar>
     );
 });
 
-
 Header.propTypes = {
     location: PropTypes.object,
 };
-
 
 export default withRouter(Header);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,27 @@
-import React, {memo} from 'react';
-import {withRouter} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { memo } from "react";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
-import HomeIcon from '@material-ui/icons/Home';
-import LocalMallIcon from '@material-ui/icons/LocalMall';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import HomeIcon from "@material-ui/icons/Home";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import InfoIcon from "@material-ui/icons/Info";
 
-import {useAppContext} from '../context/AppContext';
+import { useAppContext } from "../context/AppContext";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
@@ -29,25 +30,25 @@ const useStyles = makeStyles(theme => ({
         width: drawerWidth,
     },
     drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
+        display: "flex",
+        alignItems: "center",
         padding: theme.spacing(0, 1),
-        justifyContent: 'flex-end',
+        justifyContent: "flex-end",
         ...theme.mixins.toolbar,
-    }
+    },
 }));
 
 /**
-   * Return the Drawer Component.
-   * @param {object} history
-   * @return {any}
-**/
-const Sidebar = memo(({history}) => {
+ * Return the Drawer Component.
+ * @param {object} history
+ * @return {any}
+ **/
+const Sidebar = memo(({ history }) => {
     const classes = useStyles();
     const { state, dispatch } = useAppContext();
 
     const handleDrawerClose = () => {
-        dispatch({type: 'setOpen', value:false});
+        dispatch({ type: "setOpen", value: false });
     };
 
     return (
@@ -67,26 +68,21 @@ const Sidebar = memo(({history}) => {
             </div>
             <Divider />
             <List>
-                <ListItem button onClick={()=>history.push('/')}>
-                    <ListItemIcon>
-                        <HomeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Home" />
-                </ListItem>
-                <ListItem button onClick={()=>history.push('/products')}>
-                    <ListItemIcon>
-                        <LocalMallIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Products" />
-                </ListItem>
+                {state.pages.map((page) => {
+                    return (
+                        <ListItem key={page.id} button onClick={() => history.push(page.slug)}>
+                            <ListItemIcon>{page.slug === "/" ? <HomeIcon /> : page.slug === "/about" ? <InfoIcon /> : <MenuBookIcon />}</ListItemIcon>
+                            <ListItemText primary={page.title} />
+                        </ListItem>
+                    );
+                })}
             </List>
         </Drawer>
     );
 });
 
-
 Sidebar.propTypes = {
-    history: PropTypes.object
+    history: PropTypes.object,
 };
 
 export default withRouter(Sidebar);
