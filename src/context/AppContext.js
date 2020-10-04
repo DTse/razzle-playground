15fd8914581fb4774d04eff985e502cedccdd1e8
Dimitris Tsetsonis <dimitristsetsonis@MacBook-Pro.station>
2020-10-04 16:15:44 +0300

    feat: create app context for state management

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,51 +1,59 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useReducer, useContext, memo, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 
 const useAppContext = () => useContext(AppContext);
 
+axios.defaults.baseURL = "http://localhost:8000";
+
 // Initial app state
 let initialState = {
     open: false,
+    pages: [],
 };
 
 /**
-   * useReducer returns a callback  that updates the state.
-   * @param {string} state
-   * @param {object} action
-   * @return {any}
-**/
+ * useReducer returns a callback  that updates the state.
+ * @param {string} state
+ * @param {object} action
+ * @return {any}
+ **/
 let reducer = (state, action) => {
-  switch (action.type) {
-    case "reset":
-      return initialState;
-    case "setOpen":
-      return { ...state, open: action.value };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "reset":
+            return initialState;
+        case "setOpen":
+            return { ...state, open: action.value };
+        case "setPages":
+            return { ...state, pages: [...action.value] };
+        default:
+            return state;
+    }
 };
 
 /**
-   * Context provider.
-   * @param {object} props
-   * @return {any}
-**/
-const AppContextProvider = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const value = {state, dispatch};
+ * Context provider.
+ * @param {object} props
+ * @return {any}
+ **/
+const AppContextProvider = memo((props) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const value = { state, dispatch };
 
-  return (
-    <AppContext.Provider value={value}>
-      {props.children}
-    </AppContext.Provider>
-  );
-};
+    useEffect(() => {
+        axios.get("/pages").then((res) => {
+            dispatch({ type: "setPages", value: res.data });
+        });
+    }, []);
+
+    return <AppContext.Provider value={value}>{props.children}</AppContext.Provider>;
+});
 
 /**
-   * Context consumer.
-   * @return {any}
-**/
+ * Context consumer.
+ * @return {any}
+ **/
 let AppContextConsumer = AppContext.Consumer;
 
 export { AppContext, AppContextProvider, AppContextConsumer, useAppContext };
